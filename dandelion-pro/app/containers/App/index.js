0f1ed8639edfbe6116fe1919edbea8ff4f56e59c
muslum <mgezgin@phinion.com>
2020-01-30 12:18:25 +0100

    fix typo

--- dandelion-pro/app/containers/App/index.js
+++ dandelion-pro/app/containers/App/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import NotFound from "containers/Pages/Standalone/NotFoundDedicated";
-import Auth from "./Auth";
-import Application from "./Application";
-import LandingCorporate from "./Landing";
-import LandingCreative from "./LandingCreative";
-import ArticleNews from "./ArticleNews";
-import Users from "./Users";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import NotFound from 'containers/Pages/Standalone/NotFoundDedicated';
+import Auth from './Auth';
+import Application from './Application';
+import LandingCorporate from './Landing';
+import LandingCreative from './LandingCreative';
+import ArticleNews from './ArticleNews';
+import Users from './Users';
 
-import ThemeWrapper, { AppContext } from "./ThemeWrapper";
+import ThemeWrapper, { AppContext } from './ThemeWrapper';
 window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 
 class App extends React.Component {
