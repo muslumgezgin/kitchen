12d6db89cb9ed533fa1816241557f7722e5ed2b1
muslum <mgezgin@phinion.com>
2020-02-25 15:10:41 +0100

    add offers button

--- dandelion-pro/app/containers/Pages/BlankPage/index.js
+++ dandelion-pro/app/containers/Pages/BlankPage/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import brand from 'dan-api/dummy/brand';
 import { PapperBlock } from 'dan-components';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import css from './index.scss';
 
+const LinkBtn = React.forwardRef(function LinkBtn(props, ref) { // eslint-disable-line
+  return <Link to={props.to} {...props} innerRef={ref} />; // eslint-disable-line
+});
+
+// eslint-disable-next-line react/no-multi-comp
 class BlankPage extends React.Component {
   render() {
     const title = brand.name + ' - Blank Page';
     const description = brand.desc;
     return (
-      <div>
+      <div className={css.BlankPageComponent}>
         <Helmet>
           <title>{title}</title>
           <meta name="description" content={description} />
@@ -18,6 +26,15 @@ class BlankPage extends React.Component {
           <meta property="twitter:description" content={description} />
         </Helmet>
         <PapperBlock title="" desc="">
+          <Button
+            variant="contained"
+            color=""
+            className={css.buttonLink}
+            component={LinkBtn}
+            to="/app/forms/date-time-picker"
+          >
+            NIEUW OFFERTE &nbsp; &#x279C;
+          </Button>
           {this.props.children}
         </PapperBlock>
       </div>
