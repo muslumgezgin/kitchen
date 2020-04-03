9a9f4dc352799bfd84eaa0fc51372f0d7b4388c9
muslumgezgin <muslum_gezgin@hotmail.com>
2020-04-03 11:25:34 +0200

    change port

--- dandelion-pro/app/containers/App/Companies.js
+++ dandelion-pro/app/containers/App/Companies.js
@@ -1,81 +1,16 @@
-import React from "react";
-import { PropTypes } from "prop-types";
-import { Switch, Route } from "react-router-dom";
-import Dashboard from "../Templates/DashboardUsers";
-import dataMenu from "dan-api/ui/companyMenu";
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
+import dataMenu from 'dan-api/ui/companyMenu';
+import Dashboard from '../Templates/DashboardUsers';
 
 import {
-  Status,
-  AppLayout,
-  Responsive,
-  Grid,
-  SimpleTable,
-  AdvancedTable,
-  TablePlayground,
-  TreeTable,
-  EditableCell,
-  ReduxForm,
-  DateTimePicker,
-  CheckboxRadio,
-  Switches,
-  Selectbox,
-  Rating,
-  SliderRange,
-  Buttons,
-  DialButton,
-  ToggleButton,
-  Textbox,
-  Autocomplete,
-  Upload,
-  TextEditor,
-  Avatars,
-  Accordion,
-  Badges,
-  List,
-  PopoverTooltip,
-  Snackbar,
-  Typography,
-  Tabs,
-  Cards,
-  ImageGrid,
-  Progress,
-  DialogModal,
-  Steppers,
-  Paginations,
-  DrawerMenu,
-  Breadcrumbs,
-  Icons,
-  IonIcons,
-  SliderCarousel,
-  Tags,
-  Dividers,
-  LineCharts,
-  BarCharts,
-  AreaCharts,
-  PieCharts,
-  RadarCharts,
-  ScatterCharts,
-  CompossedCharts,
-  DoughnutCharts,
-  BarDirection,
-  LineScatterChart,
-  AreaFilledChart,
-  RadarPolarCharts,
-  Contact,
   Chat,
-  Email,
-  TaskBoard,
-  Ecommerce,
-  Timeline,
-  Calendar,
-  UserDashboardPage,
   CompaniesDashBoardPage,
   CompaniesOffersPage,
   BlankPage,
-  SearchMap,
-  TrafficIndicator,
   NotFound
-} from "../pageListAsync";
+} from '../pageListAsync';
 
 class Companies extends React.Component {
   render() {
@@ -83,13 +18,10 @@ class Companies extends React.Component {
     return (
       <Dashboard history={history} changeMode={changeMode} dataMenu={dataMenu}>
         <Switch>
-          {/* Home */}
           <Route exact path="/companies/dashboard" component={CompaniesDashBoardPage} />
           <Route path="/companies/offers" component={CompaniesOffersPage} />
           <Route path="/companies/messages" component={Chat} />
           <Route path="/app/pages/blank-page" component={BlankPage} />
-
-          {/* Default */}
           <Route component={NotFound} />
         </Switch>
       </Dashboard>
